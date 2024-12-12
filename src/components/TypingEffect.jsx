f07ed3534ff18807ffd621f49513a07d35aadffb
Harshal Patel <110181751+Harshpatel2414@users.noreply.github.com>
2024-12-13 03:44:41 +0530

    fix typing effect

--- src/components/TypingEffect.jsx
+++ src/components/TypingEffect.jsx
@@ -15,11 +15,11 @@ const TypingEffect = ({ content }) => {
     const interval = setInterval(() => {
       if (currentIndex < content.length) {
         setDisplayedContent(content.substring(0, currentIndex + 1));
-        currentIndex += 50;
+        currentIndex += 1;
       } else {
         clearInterval(interval); 
       }
-    }, 50); 
+    }, 0); 
 
     return () => clearInterval(interval); 
   }, [content]);
