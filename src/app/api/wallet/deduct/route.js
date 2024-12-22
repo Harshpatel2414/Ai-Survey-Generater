9988e4fa474143b237582bd20445081903a60b22
Harshal Patel <110181751+Harshpatel2414@users.noreply.github.com>
2024-12-23 01:35:35 +0530

    update transaction on debited

--- src/app/api/wallet/deduct/route.js
+++ src/app/api/wallet/deduct/route.js
@@ -10,6 +10,7 @@ export const POST = async (req) => {
         await client.connect();
         const db = client.db(process.env.DATABASE);
         const collection = db.collection('Users');
+        const transactionCollection = db.collection('Transactions');
         let fixedAmount = parseFloat(amount).toFixed(2);
         const result = await collection.updateOne(
             { _id: new ObjectId(userId) },
@@ -21,6 +22,13 @@ export const POST = async (req) => {
         if (result.modifiedCount === 0) {
             return NextResponse.json({ message: 'User not found or wallet update failed' }, { status: 400 });
         }
+        await transactionCollection.insertOne({
+        userId,
+        amount: fixedAmount,
+        transactionDate: new Date().toISOString(),
+        transactionType: 'debited',
+        paymentIntentId: "Generate Ai-Survey",
+      });
 
         return NextResponse.json({ message: 'Wallet updated successfully' }, { status: 200 });
     } catch (error) {
